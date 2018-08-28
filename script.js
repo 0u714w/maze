49015305e1ba75756a83acedd28a218c6b6e3b23
Doug Enas <douglassenas@gmail.com>
2018-08-28 18:32:18 -0400

    finished assignment

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const map = [
     "W         W     W W W",
     "W WWW WWWWW WWWWW W W",
     "W W   W   W W     W W",
-    "  WWWW W W W WWW W F",
+    "W WWWWW W W W WWW W F",
     "S     W W W W W W WWW",
     "WWWWW W W W W W W W W",
     "W     W W W   W W W W",
@@ -17,49 +17,104 @@ const map = [
 ];
 
 
-const maze = document.getElementById("maze")
 
+const main = document.getElementById("maze")
 
+for (let y = 0; y < map.length; y++) {
+    let row = map[y];
+    let mazeBoard = document.createElement("div");
+    mazeBoard.classList.add("mazeRow");
 
-
-
-
-for (let i = 0; i < map.length; i++) {
-    var row = map[i];
-    let mazeboard = document.createElement("div")
-    mazeboard.classList.add("mazeRow")
-
-    for (let j = 0; j < map[i].length; j++) {
+    for (let x = 0; x < row.length; x++) {
         let wall = document.createElement("div")
-        wall.dataset.rowIndex = i;
-        wall.dataset.wallIndex = j;
-        mazeboard.appendChild(wall)
+        wall.dataset.rowIndex = y;
+        wall.dataset.cellIndex = x;
+        mazeBoard.appendChild(wall);
 
-        switch (row[j]) {
+        switch (row[x]) {
             case "W":
                 wall.classList.add("borderWall")
                 wall.dataset.cellType = "border";
                 break;
 
             case "S":
-                wall.setAttribute("id", "start")
+                wall.setAttribute("id", "start");
                 wall.dataset.cellType = "start";
                 break;
 
             case " ":
                 wall.classList.add("blankSpace");
                 wall.dataset.cellType = "floor";
+                break;
 
             case "F":
                 wall.setAttribute("id", "finish");
                 wall.dataset.cellType = "end"
                 break;
         }
-
-
     }
-
-    maze.appendChild(mazeboard);
-
-
+    main.appendChild(mazeBoard);
 }
+
+let boxTop;
+let boxLeft;
+let x;
+let y;
+
+const token = document.getElementById("token")
+
+let start = document.getElementById("start")
+let currentPosition = start;
+currentPosition.appendChild(token);
+
+
+
+document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'ArrowUp':
+            let nextPositionUp = Number(currentPosition.dataset.rowIndex) - 1;
+            let nextMoveUp = document.querySelector("[data-row-index = '" + nextPositionUp + "'][data-cell-index = '" + currentPosition.dataset.cellIndex + "']");
+            if (nextMoveUp.dataset.cellType === "floor") {
+                nextMoveUp.appendChild(token);
+                currentPosition = nextMoveUp;
+                token.style.transform = "rotate(-90deg)"
+            }
+            break;
+        case 'ArrowDown':
+            let nextPositionDown = Number(currentPosition.dataset.rowIndex) + 1;
+            let nextMoveDown = document.querySelector("[data-row-index = '" + nextPositionDown + "'][data-cell-index = '" + currentPosition.dataset.cellIndex + "']");
+            if (nextMoveDown.dataset.cellType === "floor") {
+                nextMoveDown.appendChild(token);
+                currentPosition = nextMoveDown;
+                token.style.transform = "rotate(90deg)"
+            }
+            break;
+        case 'ArrowLeft':
+            let nextPositionLeft = Number(currentPosition.dataset.cellIndex) - 1;
+            let nextMoveLeft = document.querySelector("[data-row-index = '" + currentPosition.dataset.rowIndex + "'][data-cell-index = '" + nextPositionLeft + "']");
+            if (nextMoveLeft.dataset.cellType === "floor") {
+                nextMoveLeft.appendChild(token);
+                currentPosition = nextMoveLeft;
+                token.style.transform = "scaleX(-1)"
+            }
+            break;
+        case 'ArrowRight':
+            let nextPositionRight = Number(currentPosition.dataset.cellIndex) + 1;
+            let nextMoveRight = document.querySelector("[data-row-index = '" + currentPosition.dataset.rowIndex + "'][data-cell-index = '" + nextPositionRight + "']");
+            if (nextMoveRight.dataset.cellType === "floor") {
+                nextMoveRight.appendChild(token);
+                currentPosition = nextMoveRight;
+                token.style.transform = "rotate(0deg)"
+            } else if (nextMoveRight.dataset.cellType === "end") {
+                nextMoveRight.appendChild(token);
+                currentPosition = nextMoveRight;
+                setTimeout(function() {
+                    alert("You Win!");
+
+                }, 1);
+            }
+            break;
+    }
+    document.getElementById("token").style.top = boxTop + "px";
+    document.getElementById("token").style.left = boxLeft + "px";
+})
